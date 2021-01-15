b01e3ebe708b7fb4949b1078fcd4e3d5149a2ebf
oussamaalmesskine <oussama.almesskine@gmail.com>
2021-01-15 02:54:25 +0100

    Update2

--- Step 2/2.c
+++ Step 2/2.c
@@ -11,3 +11,75 @@
 * Créez votre arbre de décision
 
 Refaite le meme exercice en utilisant `switch Case` */
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main() {
+
+  //Variables
+  float a, b;
+  char operator;
+  double result;
+  
+  //Entree
+  printf("Entrer le nombre 1:\n");
+    scanf("%f", &a);
+
+    printf("Entrer le nombre 2:\n");
+    scanf("%f", &b);
+
+
+  //Affichage de leur taille en Octet et en Hexadecimal
+  printf("number 1 (%f) : \n\t- Octal : %f\n\t- Hex : %x",a, a, (unsigned char)a);
+  printf("number 2 (%f) : \n\t- Octal : %f\n\t- Hex : %x",b, b, (unsigned char)b);
+
+
+   //Entree
+  printf("\n Donnez l'operateur : ");
+  scanf(" %c", &operator);
+
+
+  //Traitement(Calcul)
+  switch (operator)
+    {
+        case '*' :
+            result = a * b;
+            break;
+        
+        case '+' :
+            result = a + b;
+            break;
+        
+        case '-' :
+            result = a - b;
+            break;
+        
+        case '/'  :{ if (b!=0)
+            result = a / b;
+            else
+                printf("Error calcul\n");
+                exit(2);
+            
+            break;}  
+
+        case '%' :{ if (b!=0)
+            result = (int)a % (int)b;
+            else
+                printf("Error calcul\n");
+                exit(2);
+            break;}
+      default:
+          printf("\nError! operator is not correct");
+          exit(2);
+          break;
+    }
+
+
+   // Result
+    printf("\nLa Resultat est : %.2lf\n", result);
+   
+  return 0;
+}
